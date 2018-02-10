95d09cf297a1dfd1893480da7cbd226b8da83024
Yuta Katayama <uosotm@gmail.com>
2018-02-10 16:14:40 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='Ciclustering',
+    description='An automation tool for Collective Idea',
+    author='Yuta Katayama',
+    author_email='uosotm@gmail.com',
+    version='0.1.0',
+    py_modules=['ciclustering'],
+    install_requires=[
+        'click',
+    ],
+    entry_points={'console_scripts': ['ciclustering=main:cli'], },
+)
