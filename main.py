7c6265076e3cb2efda4f74964c5e0bba7dd151c5
Yuta Katayama <uosotm@gmail.com>
2018-02-10 17:32:59 +0900

    Change to create a list of path to images

--- main.py
+++ main.py
@@ -20,22 +20,25 @@ def print_version(ctx, param, value):
 def cli(dest, images_path):
     """CIclustering is an automation tool for Collective Idea"""
 
+    # Validat path to images
     images_path = pathlib.Path(images_path)
-
     if not images_path.exists():
         click.secho('Error: No such directory', fg='red') 
         sys.exit(1)
 
     if not images_path.is_dir():
-        click.secho('Error: {} isn\'t directory'.format(str(images)), fg='red')
+        click.secho('Error: {} isn\'t directory'.format(str(images_path)), fg='red')
         sys.exit(1)
 
     click.echo('Path to input files: {}'.format(images_path.resolve()))
 
-    images = images_path.glob('*.jpg')
-    length = sum(1 for image in images)
-    click.echo('Reading files for process: {} media'.format(length))
+    # TODO: Validate and create a directory for output files
+    
+    # Count only jpg images in path
+    images = [image for image in images_path.glob('*.jpg')]
+    click.echo('Reading files for process: {} media'.format(len(images)))
 
+    # TODO: Add progress bar to show image rcognition progress
     for image in images:
         click.echo(image)
 
