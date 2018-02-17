7af4018ead3c9cae765085abe7ccde7e98133764
Yuta Katayama <uosotm@gmail.com>
2018-02-17 18:20:31 +0900

    Add basic api requesting

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pathlib
 import requests
 import sys
 import time
+import urllib
 from datetime import datetime
 from tqdm import tqdm
 
@@ -28,14 +29,13 @@ def load_config(ctx, param, value):
         else:
             ctx.exit('Exit.')
 
-    config.read(config_path, encoding='utf8')
+    config.read(str(config_path), encoding='utf8')
     return config
 
 def new_config(config_path, config):
     cva_url = click.prompt('\nRequests to',
             default='https://vision.googleapis.com/v1/images:annotate')
     cva_key = click.prompt('Please enter your API KEY')
-    
     config['default'] = { 'cva_url': cva_url,
                           'cva_key': cva_key }
 
@@ -43,17 +43,15 @@ def new_config(config_path, config):
         config.write(configfile)
         click.echo('\nSaving config...')
 
-
     click.secho('Successfuly created the config file!',
             bold=True, fg='green')
 
 def encode_image(image):
     image_content = image.read()
-    return base.b64encode(image_content).decode('UTF-8')
+    return base64.b64encode(image_content).decode('UTF-8')
 
 def create_request(encoded_image):
     request_list = []
-
     content_json_obj = {
         'content': encoded_image
     }
@@ -61,7 +59,7 @@ def create_request(encoded_image):
     feature_json_obj = []
     feature_json_obj.append({
         'type': 'LABEL_DETECTION',
-        'maxResults': 1
+        'maxResults': 10
     })
 
     request_list.append({
@@ -70,7 +68,9 @@ def create_request(encoded_image):
     })
     return { 'requests': request_list }
 
-def upload(url, data):
+def upload(url, key, data):
+    args = { 'key': key }
+    url = url + "?{}".format(urllib.parse.urlencode(args))
     r = requests.post(url, json=data)
     return r.text
 
@@ -104,6 +104,7 @@ def cli(config, dest, images_path):
     dest = pathlib.Path(dest)
     try:
         dest.mkdir(parents=True, exist_ok=True)
+
     except FileExistsError:
         click.secho('Error: {} isn\'t directory'.format(dest.resolve()),
                     fg='red', err=True)
@@ -113,10 +114,16 @@ def cli(config, dest, images_path):
     # Count only jpg images in path
     images = [image for image in images_path.glob('*.jpg')]
     length = len(images)
-    click.echo('Reading files for process: {} media'.format(length))
+    click.echo('Reading files for process: {} media\n'.format(length))
+
+    cva_url = config['default']['cva_url']
+    cva_key = config['default']['cva_key']
 
     for image in tqdm(images, desc='Recognising', total=length, ncols=10):
-        # Recognising code goes here.
-        time.sleep(0.1)
+        with image.open('rb') as imagefile:
+            encoded_image = encode_image(imagefile)
+            payload = create_request(encoded_image)
+            res = upload(cva_url, cva_key, payload)
+            print(res)
 
     click.secho('Done!', bold=True, fg='green')
