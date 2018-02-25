ff6e1f6aa08358b5d75be1b054dff6f31d1b39ca
Yuta Katayama <uosotm@gmail.com>
2018-02-25 21:44:30 +0900

    Add ability to recognize number of humans

--- ciclustering/main.py
+++ ciclustering/main.py
@@ -56,17 +56,22 @@ def encode_image(image):
     image_content = image.read()
     return base64.b64encode(image_content).decode('UTF-8')
 
-def create_request(encoded_image):
+def create_request(encoded_image, mode):
     request_list = []
     content_json_obj = {
         'content': encoded_image
     }
 
     feature_json_obj = []
-    feature_json_obj.append({
-        'type': 'LABEL_DETECTION',
-        'maxResults': 10
-    })
+    if mode == 'object':
+        feature_json_obj.append({
+            'type': 'LABEL_DETECTION',
+            'maxResults': 10
+        })
+    else:
+        feature_json_obj.append({
+            'type': 'FACE_DETECTION'
+        })
 
     request_list.append({
         'features': feature_json_obj,
@@ -99,77 +104,131 @@ def main(config, dest, mode, images_path):
     # Initialize recognition mode
     if mode == 'object':
         keyword = click.prompt('Please enter a keyward')
-    else:
-        click.echo('Not supported currently.')
-        sys.exit(0)
 
-    # Validate path to images
-    images_path = pathlib.Path(images_path)
-    if not images_path.exists():
-        click.secho('Error: No such directory',
-                    fg='red', err=True) 
-        sys.exit(1)
+        # Validate path to images
+        images_path = pathlib.Path(images_path)
+        if not images_path.exists():
+            click.secho('Error: No such directory',
+                        fg='red', err=True) 
+            sys.exit(1)
+        if not images_path.is_dir():
+            click.secho('Error: {} isn\'t directory'.format(str(images_path)),
+                        fg='red', err=True)
+            sys.exit(1)
+        click.echo('Path to target directory: {}'.format(images_path.resolve()))
 
-    if not images_path.is_dir():
-        click.secho('Error: {} isn\'t directory'.format(str(images_path)),
-                    fg='red', err=True)
-        sys.exit(1)
+        # Validate and create a directory for output files
+        dest = pathlib.Path(dest)
+        dest_keyword = dest / keyword
+        dest_others = dest / 'others'
+        try:
+            dest.mkdir(parents=True, exist_ok=True)
+            dest_keyword.mkdir(parents=True, exist_ok=True)
+            dest_others.mkdir(parents=True, exist_ok=True)
 
-    click.echo('Path to target directory: {}'.format(images_path.resolve()))
+        except FileExistsError:
+            click.secho('Error: {} isn\'t directory'.format(dest.resolve()),
+                        fg='red', err=True)
+            sys.exit(1)
+        click.echo('Created a directory for results: {}'.format(dest.resolve()))
+        
+        # Count only jpg images in path
+        images = [image for image in images_path.glob('*.jpg')]
+        length = len(images)
+        click.echo('Reading files for process: {} media\n'.format(length))
 
-    # Validate and create a directory for output files
-    dest = pathlib.Path(dest)
-    dest_keyword = dest / keyword
-    dest_others = dest / 'others'
-    try:
-        dest.mkdir(parents=True, exist_ok=True)
-        dest_keyword.mkdir(parents=True, exist_ok=True)
-        dest_others.mkdir(parents=True, exist_ok=True)
+        cva_url = config['default']['cva_url']
+        cva_key = config['default']['cva_key']
 
-    except FileExistsError:
-        click.secho('Error: {} isn\'t directory'.format(dest.resolve()),
-                    fg='red', err=True)
-        sys.exit(1)
+        with (dest / 'results.csv').open('w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
 
-    click.echo('Created a directory for results: {}'.format(dest.resolve()))
-    
-    # Count only jpg images in path
-    images = [image for image in images_path.glob('*.jpg')]
-    length = len(images)
-    click.echo('Reading files for process: {} media\n'.format(length))
+            for image in tqdm(images, desc='Recognising', total=length, ncols=10):
+                row = [str(image)]
+                with image.open('rb') as imagefile:
+                    encoded_image = encode_image(imagefile)
 
-    cva_url = config['default']['cva_url']
-    cva_key = config['default']['cva_key']
+                payload = create_request(encoded_image, mode)
+                res = upload(cva_url, cva_key, payload)
 
-    with (dest / 'results.csv').open('w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
+                # Organize files to proper directory 
+                try:
+                    labels = json.loads(res)['responses'][0]['labelAnnotations']
+                except KeyError:
+                    writer.writerow(row)
+                    continue
 
-        for image in tqdm(images, desc='Recognising', total=length, ncols=10):
-            row = [str(image)]
-            with image.open('rb') as imagefile:
-                encoded_image = encode_image(imagefile)
+                descriptions = [label['description'] for label in labels]
+                scores = [label['score'] for label in labels]
+                if keyword in descriptions:
+                    shutil.move(str(image), str(dest_keyword))
+                else:
+                    shutil.move(str(image), str(dest_others))
 
-            payload = create_request(encoded_image)
-            res = upload(cva_url, cva_key, payload)
+                for name, score in zip(descriptions, scores):
+                    row.append('{}({})'.format(name, score))
 
-            # Organize files to proper directory 
-            try:
-                labels = json.loads(res)['responses'][0]['labelAnnotations']
-            except KeyError:
-                click.echo('No label annotations for {}'.format(str(image)))
                 writer.writerow(row)
-                continue
 
-            descriptions = [label['description'] for label in labels]
-            scores = [label['score'] for label in labels]
-            if keyword in descriptions:
-                shutil.move(str(image), str(dest_keyword))
-            else:
-                shutil.move(str(image), str(dest_others))
+    else:
+        # Validate path to images
+        images_path = pathlib.Path(images_path)
+        if not images_path.exists():
+            click.secho('Error: No such directory',
+                        fg='red', err=True) 
+            sys.exit(1)
+        if not images_path.is_dir():
+            click.secho('Error: {} isn\'t directory'.format(str(images_path)),
+                        fg='red', err=True)
+            sys.exit(1)
+        click.echo('Path to target directory: {}'.format(images_path.resolve()))
 
-            for name, score in zip(descriptions, scores):
-                row.append('{}({})'.format(name, score))
+        # Validate and create a directory for output files
+        dest = pathlib.Path(dest)
+        try:
+            dest.mkdir(parents=True, exist_ok=True)
+        except FileExistsError:
+            click.secho('Error: {} isn\'t directory'.format(dest.resolve()),
+                        fg='red', err=True)
+            sys.exit(1)
+        click.echo('Created a directory for results: {}'.format(dest.resolve()))
 
-            writer.writerow(row)
+        # Count only jpg images in path
+        images = [image for image in images_path.glob('*.jpg')]
+        length = len(images)
+        click.echo('Reading files for process: {} media\n'.format(length))
+
+        cva_url = config['default']['cva_url']
+        cva_key = config['default']['cva_key']
+
+        with (dest / 'results.csv').open('w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+
+            for image in tqdm(images, desc='Recognising', total=length, ncols=10):
+                row = [str(image)]
+                with image.open('rb') as imagefile:
+                    encoded_image = encode_image(imagefile)
+
+                payload = create_request(encoded_image, mode)
+                res = upload(cva_url, cva_key, payload)
+
+                # Organize files to proper directory 
+                try:
+                    faces = len(json.loads(res)['responses'][0]['faceAnnotations'])
+                except KeyError:
+                    faces = 0
+
+                dest_number = pathlib.Path(dest) / str(faces)
+                try:
+                    dest_number.mkdir(parents=True, exist_ok=True)
+                except FileExistsError:
+                    click.secho('Error: {} isn\'t directory'.format(dest_number.resolve()),
+                                fg='red', err=True)
+                    sys.exit(1)
+
+                shutil.move(str(image), str(dest_number))
+                row.append('{}'.format(faces))
+                writer.writerow(row)
+
 
     click.secho('Done!', bold=True, fg='green')
